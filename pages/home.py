3aa2fe5e6c7882eec12470e46a2fa3bca450d93d
BluDolphin <github.com.tablet296@passmail.net>
2025-11-25 04:08:43 +0000

    minor comments

--- pages/home.py
+++ pages/home.py
@@ -5,6 +5,7 @@ from pages.theme import frame
 from pages.login import check_password
 
 def content():
+    # Start the chatbot service
     def start_bot(input_password):
         if not check_password(input_password):
             return ui.notify('Incorrect password', color='red')
@@ -12,18 +13,21 @@ def content():
         threading.Thread(target=main_sms.start_service, args=(input_password,)).start() # start in new thread
         start_bot_dialog.close() # Close dialog
         return
+    
+    # Stop the chatbot service
     def stop_bot():
         main_sms.stop_service()
         return
     
+    # Dialog for starting the bot
     with ui.dialog() as start_bot_dialog, ui.card():
-        ui.label('Start Chatbot Service').classes('text-lg mt-2')
-        hash_key_input = ui.input('Enter password:', password=True)
-        with ui.row().classes('justify-end mt-4 mb-2'):
-            ui.button('Close', on_click=start_bot_dialog.close)
-            ui.button('Start', on_click=lambda: start_bot(hash_key_input.value)).classes('ml-2')
-
-        
+        ui.label('Start Chatbot Service').classes('text-lg mt-2') # Dialog title
+        hash_key_input = ui.input('Enter password:', password=True) # Input for password
+        with ui.row().classes('justify-end mt-4 mb-2'): # Row for buttons
+            ui.button('Close', on_click=start_bot_dialog.close) # Close button
+            ui.button('Start', on_click=lambda: start_bot(hash_key_input.value)).classes('ml-2') # Start button
+    
+    # Main content within the themed frame
     with frame('Home'):
         ui.label('Welcome to BluChat').classes('text-2xl mt-2')
         with ui.row().classes('w-full no-wrap gap-0'):
--- pages/settings.py
+++ pages/settings.py
@@ -3,7 +3,8 @@ import os
 from pages.theme import frame
 
 def content():
+    # Main content within the themed frame
     with frame('Settings'):
         ui.label('Settings').classes('text-2xl mt-2')
-    
+
                     
